b8978609fa70b4a6c24119d24cd4c664a3332b4c
Jesper Samson <jrsamson@uerm.edu.ph>
2025-11-18 10:09:57 +0800

    convert to options api index.vue

--- todolist-app/src/pages/IndexPage.vue
+++ todolist-app/src/pages/IndexPage.vue
@@ -71,158 +71,176 @@
 }
 </style>
 
-<script setup>
-import { ref, onMounted } from "vue";
+<script>
 import axios from "axios";
-import { useQuasar } from "quasar";
+// import { date } from "quasar";
 import TodoItem from "components/TodoItem.vue";
 import { useTaskStore } from "src/composable/useTaskStore.js";
 
-const $q = useQuasar();
-const newTodo = ref("");
-const newTodoDueDate = ref(null);
-const todos = ref([]);
-
-const editingTodoId = ref(null);
-const editingTodotaskName = ref("");
-const editingTodoDueDate = ref(null);
-
-const {
-  fetchTaskCount,
-  incrementCount,
-  decrementCount,
-  incrementCompletedCount,
-  decrementCompletedCount,
-} = useTaskStore();
-
 const API_URL = "http://localhost:3000/tbl_tasks";
 
-onMounted(() => {
-  fetchTodos();
-  fetchTaskCount();
-});
+export default {
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      newTodo: "",
+      newTodoDueDate: "",
+      todos: [],
+      editingTodoId: null,
+      editingTodotaskName: "",
+      editingTodoDueDate: "",
+    };
+  },
+  setup() {
+    const {
+      fetchTaskCount,
+      incrementCount,
+      decrementCount,
+      incrementCompletedCount,
+      decrementCompletedCount,
+    } = useTaskStore();
 
-async function fetchTodos() {
-  try {
-    const response = await axios.get(API_URL);
-    todos.value = response.data.filter((task) => task && task.id);
-  } catch (error) {
-    console.error("Error fetching todos:", error);
-  }
-}
+    return {
+      fetchTaskCount,
+      incrementCount,
+      decrementCount,
+      incrementCompletedCount,
+      decrementCompletedCount,
+    };
+  },
 
-async function addTodo() {
-  if (newTodo.value.trim() !== "") {
-    try {
-      const response = await axios.post(API_URL, {
-        taskName: newTodo.value,
-        dueDate: newTodoDueDate.value,
-      });
-      if (response.data && response.data.id) {
-        $q.notify({
-          message: "New task added successfully!",
-          color: "positive",
-          icon: "check_circle",
+  methods: {
+    async fetchTodos() {
+      try {
+        const response = await axios.get(API_URL);
+        this.todos = response.data.filter((task) => task && task.id);
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    },
+    async addTodo() {
+      if (this.newTodo.trim() !== "") {
+        try {
+          const response = await axios.post(API_URL, {
+            taskName: this.newTodo,
+            dueDate: this.newTodoDueDate,
+          });
+          if (response.data && response.data.id) {
+            this.$q.notify({
+              message: "New task added successfully!",
+              color: "positive",
+              icon: "check_circle",
+              position: "top",
+            });
+            this.todos.unshift(response.data);
+            this.incrementCount();
+            this.newTodo = "";
+            this.newTodoDueDate = null;
+          } else {
+            throw new Error("Invalid response from server");
+          }
+        } catch (error) {
+          console.error("Error adding todo:", error);
+        }
+      } else {
+        this.$q.notify({
+          message: "Please fill task field.",
+          color: "negative",
+          icon: "warning",
           position: "top",
         });
-        todos.value.unshift(response.data);
-        incrementCount();
-        newTodo.value = "";
-        newTodoDueDate.value = null;
-      } else {
-        throw new Error("Invalid response from server");
       }
-    } catch (error) {
-      console.error("Error adding todo:", error);
-    }
-  } else {
-    $q.notify({
-      message: "Please fill task field.",
-      color: "negative",
-      icon: "warning",
-      position: "top",
-    });
-  }
-}
-
-async function updateTodo(todo) {
-  try {
-    await axios.put(`${API_URL}/${todo.id}`, todo);
-  } catch (error) {
-    console.error("Error updating todo:", error);
-  }
-}
-
-function toggleTodoStatus(id) {
-  const todo = todos.value.find((t) => t.id === id);
-  if (todo) {
-    todo.is_done = !todo.is_done;
-
-    if (todo.is_done) {
-      todo.date_completed = new Date();
-      decrementCount();
-      incrementCompletedCount();
-    } else {
-      todo.date_completed = null;
-      incrementCount();
-      decrementCompletedCount();
-    }
-    updateTodo(todo);
-  }
-}
-
-function startEdit(todo) {
-  editingTodoId.value = todo.id;
-  editingTodotaskName.value = todo.taskName;
-  editingTodoDueDate.value = todo.dueDate;
-}
-
-function saveEdit() {
-  if (editingTodoId.value === null) return;
-  const todo = todos.value.find((t) => t.id === editingTodoId.value);
-
-  if (todo && editingTodotaskName.value.trim()) {
-    todo.taskName = editingTodotaskName.value.trim();
-    todo.dueDate = editingTodoDueDate.value;
-    updateTodo(todo);
-  }
-  cancelEdit();
-}
-
-function cancelEdit() {
-  editingTodoId.value = null;
-  editingTodotaskName.value = "";
-  editingTodoDueDate.value = null;
-}
-
-async function deleteTodo(id) {
-  $q.dialog({
-    title: "Confirm Delete",
-    message: "Are you sure you want to permanently delete this task?",
-    ok: {
-      label: "YES",
-      color: "negative",
-      flat: false,
     },
-    cancel: {
-      label: "NO",
-      color: "white",
-      textColor: "black",
-      flat: false,
-    },
-  }).onOk(async () => {
-    try {
-      await axios.delete(`${API_URL}/${id}`);
-      const index = todos.value.findIndex((t) => t.id === id);
 
-      if (index !== -1) {
-        todos.value.splice(index, 1);
-        decrementCount();
+    async updateTodo(todo) {
+      try {
+        await axios.put(`${API_URL}/${todo.id}`, todo);
+      } catch (error) {
+        console.error("Error updating todo:", error);
       }
-    } catch (error) {
-      console.error("Error deleting todo:", error);
-    }
-  });
-}
+    },
+
+    toggleTodoStatus(id) {
+      const todo = this.todos.find((t) => t.id === id);
+      if (todo) {
+        todo.is_done = !todo.is_done;
+
+        if (todo.is_done) {
+          todo.date_completed = new Date().toISOString;
+          this.decrementCount();
+          this.incrementCompletedCount();
+        } else {
+          todo.date_completed = null;
+          this.incrementCount();
+          this.decrementCompletedCount();
+        }
+        this.updateTodo(todo);
+      }
+    },
+
+    startEdit(todo) {
+      this.editingTodoId = todo.id;
+      this.editingTodotaskName = todo.taskName;
+      this.editingTodoDueDate = todo.dueDate;
+    },
+
+    saveEdit() {
+      if (this.editingTodoId === null) return;
+      const todo = this.todos.find((t) => t.id === this.editingTodoId);
+
+      if (todo && this.editingTodotaskName.trim()) {
+        todo.taskName = this.editingTodotaskName.trim();
+        todo.dueDate = this.editingTodoDueDate;
+        this.updateTodo(todo);
+      }
+      this.cancelEdit();
+    },
+
+    cancelEdit() {
+      this.editingTodoId = null;
+      this.editingTodotaskName = "";
+      this.editingTodoDueDate = null;
+    },
+
+    async deleteTodo(id) {
+      this.$q
+        .dialog({
+          title: "Confirm Delete",
+          message: "Are you sure you want to permanently delete this task?",
+          ok: {
+            label: "YES",
+            color: "negative",
+            flat: false,
+          },
+          cancel: {
+            label: "NO",
+            color: "white",
+            textColor: "black",
+            flat: false,
+          },
+        })
+        .onOk(async () => {
+          try {
+            await axios.delete(`${API_URL}/${id}`);
+            const index = this.todos.findIndex((t) => t.id === id);
+
+            if (index !== -1) {
+              this.todos.splice(index, 1);
+              this.decrementCount();
+            }
+          } catch (error) {
+            console.error("Error deleting todo:", error);
+          }
+        });
+    },
+  },
+
+  mounted() {
+    this.fetchTodos();
+    this.fetchTaskCount();
+  },
+};
 </script>
 <style scoped></style>
